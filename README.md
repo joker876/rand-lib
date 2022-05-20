f6647f711d449f2e760410729ca6611a2f0f7af9
joker876 <42680429+joker876@users.noreply.github.com>
2022-05-20 19:17:02 +0200

    version 4.0.0

--- README.md
+++ README.md
@@ -1,2 +1,211 @@
-# take-chance
+# Take(A)Chance
 A JavaScript library to help with various tasks involving randomness.
+
+## Highlights
+* Supports TypeScript!
+* Supports Node and browser
+* Includes full JSX documentation
+* Code weighs only 16kB
+
+## Installation
+```
+npm install take-chance --save
+```
+
+## Usage
+```js
+import { TakeChance } from 'take-chance';
+```
+
+### Functions - Table of Contents
+  - [int](#int)
+  - [float](#float)
+  - [multipleInt](#multipleint)
+  - [multipleFloat](#multiplefloat)
+  - [boolean](#boolean)
+  - [binomial](#binomial)
+  - [character](#character)
+  - [string](#string)
+  - [fromArray](#fromarray)
+  - [fromObject](#fromobject)
+  - [date](#date)
+  - [id](#id)
+  - [die](#die)
+  - [rgbColor](#rgbcolor-and-hexcolor)
+  - [hexColor](#rgbcolor-and-hexcolor)
+
+#### int
+Generates a random integer, optionally specifying minimum and maximum range.
+
+Type: `function(min?: number, max?: number): number`
+
+* `min` (number) - minimum value, inclusive. Optional, default 0.
+* `max` (number) - maximum value, inclusive. Optional, default 100.
+
+```js
+TakeChance.int()            // example outputs: 78, 9, 36
+TakeChance.int(10)          // example outputs: 45, 11, 97
+TakeChance.int(0, 20)       // example outputs: 1, 13, 7
+```
+
+#### float
+Generates a random floating point decimal, optionally specifying minimum and maximum range.
+
+Type: `function(min?: number, max?: number): number`
+
+* `min` (number) - minimum value, inclusive. Optional, default 0.
+* `max` (number) - maximum value, inclusive. Optional, default 1.
+
+```js
+TakeChance.float()            // example output: 0.4089221334106232
+TakeChance.float(0.5)         // example output: 0.9873850867986081
+TakeChance.float(0, 5)        // example output: 3.4504321331627576
+```
+
+#### multipleInt
+Generates an array of random integers of defined length, optionally specifying minimum and maximum range.
+
+Type: `function(length: number, min?: number, max?: number): number[]`
+
+* `length` (number) - how many numbers to generate. Required.
+* `min` (number) - minimum value, inclusive. Optional, default 0.
+* `max` (number) - maximum value, inclusive. Optional, default 100.
+
+```js
+TakeChance.multipleInt()               // error: specify array length
+TakeChance.multipleInt(3)              // example output: [45, 11, 97]
+TakeChance.multipleInt(3, 0, 20)       // example output: [1, 13, 7]
+```
+
+#### multipleFloat
+Generates an array of random floating point decimals of defined length, optionally specifying minimum and maximum range.
+
+Type: `function(length: number, min?: number, max?: number): number[]`
+
+* `length` (number) - how many numbers to generate. Required.
+* `min` (number) - minimum value, inclusive. Optional, default 0.
+* `max` (number) - maximum value, inclusive. Optional, default 100.
+
+```js
+TakeChance.multipleFloat()           // error: specify array length
+TakeChance.multipleFloat(3)          
+TakeChance.multipleFloat(3, 0, 2)    
+```
+
+#### boolean
+Generates a random boolean value, optionally specyfing the probability of getting `true`.
+
+Type: `function(probability?: number): boolean`
+
+* `probability` (number) - the probability of getting `true`. It must be between 0 and 1. Optional, default to 0.5.
+
+```js
+TakeChance.boolean()        // example output: true (50% chance of true)
+TakeChance.boolean(0.2)     // example output: false (20% chance of true)
+```
+
+#### binomial
+Generates a random floating point decimal according to [binomial distribution](https://en.wikipedia.org/wiki/Binomial_distribution), optionally specifying minimum and maximum range, and the skew factor.
+
+Type: `function(min?: number, max?: number, skew?: number): number`
+
+* `min` (number) - minimum value, inclusive. Optional, default 0.
+* `max` (number) - maximum value, inclusive. Optional, default 1.
+* `skew` (number) - the skew factor. Must be higher than 0 to work properly (values above 10 are not really useful). Optional, default 1 (no skew). Changes how much the values are skewed towards `min` or `max`. [visualization](https://jsfiddle.net/2uc346hp/)
+
+#### character
+Generates a random character from a defined set of characters.
+
+Type: `function(options: object): string`
+
+* `options` (object) - the options to select the character. Its an object that should contain at least one of the following properties:
+  * `from` (string | string[]) - a string of characters or an array of individual characters to choose from, in addition to other options.
+  * `letters` (boolean) - if set to `true`, all letters of both cases (a-z and A-Z) are added to the characters to choose from.
+  * `uppercase` (boolean) - if set to `true`, all uppercase letters (A-Z) are added to the characters to choose from.
+  * `lowercase` (boolean) - if set to `true`, all lowercase letters (a-z) are added to the characters to choose from.
+  * `numbers` (boolean) - if set to `true`, all numbers (0-9) are added to the characters to choose from.
+  * `special` (boolean) - if set to `true`, all all ASCII special characters are added to the characters to choose from.
+
+```js
+TakeChance.character({ letters: true })       // example outputs: 'A', 't', 'Q'
+TakeChance.character({ from: 'abcdef%!' })    // example outputs: '!', 'a', 'f'
+```
+
+#### string
+Generates a random string of characters from a defined set of characters.
+
+Type: `function(length: number, options: object): string`
+* `length` (number) - the length of the string to generate. Required.
+* `options` (object) - the options to select the character. See [character](#character) for more info.
+```js
+TakeChance.string(5, { letters: true })   // example outputs: 'AviRp', 'TffPQ'
+```
+
+#### fromArray
+Picks a random element from an array.
+
+Type: `function(array: any[], min?: number, max?: number): any`
+* `array` (any[]) - the array to pick from. Required.
+* `min` (number) - the minimum index to choose from. Optional, default 0.
+* `max` (number) - the maximum index to choose from. Optional, default to last index.
+```js
+TakeChance.fromArray(['foo', 'bar', 'baz', 5])     // example output: 'bar'
+```
+
+#### fromObject
+Picks a random key from an object.
+
+Type: `function(object: object): any`
+* `object` (object) - the object to pick the key from. Required.
+```js
+TakeChance.fromArray(['foo', 'bar', 'baz', 5])     // example output: 'bar'
+```
+
+#### date
+Picks a date from between two dates.
+
+Type: `function(min: Date, max?: Date): Date`
+* `min` (Date) - the minimum date to choose from. Required.
+* `max` (Date) - the maximum date to choose from. Optional, default to current date.
+
+#### id
+Generates a random string of characters that is a [valid HTML5 id](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/id#sect2).
+
+It generates ids from these characters: a-z, A-Z, 0-9, "_", and "-". The generated id always starts with a letter.
+
+With the length of 10, there are over 4 quadrillion (4x10^15) possible ids, which makes the chance of getting two same ids extremely unlikely.
+
+Type: `function(length?: number): string`
+
+* `length` (number) - the length of the generated id. Optional, default 10.
+
+```js
+TakeChance.id()   // example output: 'e4Jwbn-3_qP'
+```
+
+#### die
+Rolls a virtual die with *n* sides. Returns a random number between 1 and *n* (inclusive).
+
+Type: `function(n?: number): string`
+* `n` (number) - the amount of sides the die has. Optional, default 6.
+
+```js
+TakeChance.die()     // example output: 5
+TakeChance.die(24)   // example output: 17
+```
+
+#### rgbColor and hexColor
+Generates a random color in either RGB or HEX format.
+
+Type: `function(): { r: number, g: number, b: number }` and `function(): string`
+
+* This function has no parameters.
+
+Function `rgbColor` returns an object containing `r`, `g`, and `b` properties, all with a random integer between 0 and 255 (inclusive).
+
+Function `hexColor` returns a string with 6 random hex symbols (lowercase letters), 2 for each color. It also adds a hash symbol (`#`) at the beginning of the string.
+
+```js
+TakeChance.rgbColor()   // example output: { r: 53, g: 187, b: 241 }
+TakeChance.hexColor()   // example output: '#a7f14e'
+```
---
+++ src/index.ts
@@ -0,0 +1,212 @@
+/**
+ * Generates a random decimal number, chosen from between two numbers.
+ * @param min The minimum number to generate. Optional, defaults to 0.
+ * @param max The maximum number to generate. Optional, defaults to 1.
+ * @returns A random decimal number between `min` and `max`.
+ */
+function float(min: number = 0, max: number = 1): number {
+    return Math.random() * (max - min) + min;
+}
+/**
+ * Generates a random integer, chosen from between two numbers.
+ * @param min The minimum number to generate. Optional, defaults to 0.
+ * @param max The maximum number to generate. Optional, defaults to 100.
+ * @returns A random integer between `min` and `max`.
+ */
+function int(min: number = 0, max: number = 100): number {
+    return Math.round(float(min, max));
+}
+/**
+ * Generates a random boolean value, with customizable probability of returning `true`.
+ * @param probability The probability of returning `true`. Should be a number between 0 and 1. Defaults to 50% (0.5).
+ * @returns A randomly generated boolean.
+ */
+function boolean(probability: number = 0.5): boolean {
+    return Math.random() < probability;
+}
+/**
+ * Generates an array of random integers, chosen from between two numbers.
+ * @param length How many numbers to generate. Required.
+ * @param min The minimum number to generate. Optional, defaults to 0.
+ * @param max The maximum number to generate. Optional, defaults to 100.
+ * @returns A an array of random integers between `min` and `max`.
+ */
+function multipleInt(length: number, min: number = 0, max: number = 100): number[] {
+    let arr: number[] = [];
+    for (let i = 0; i < length; i++) {
+        arr.push(int(min, max));
+    }
+    return arr;
+}
+/**
+ * Generates an array of random decimal numbers, chosen from between two numbers.
+ * @param length How many numbers to generate. Required.
+ * @param min The minimum number to generate. Optional, defaults to 0.
+ * @param max The maximum number to generate. Optional, defaults to 1.
+ * @returns A an array of random decimal numbers between `min` and `max`.
+ */
+function multipleFloat(length: number, min: number = 0, max: number = 1): number[] {
+    let arr: number[] = [];
+    for (let i = 0; i < length; i++) {
+        arr.push(float(min, max));
+    }
+    return arr;
+}
+/**
+ * Generates a random number, chosen from between two numbers, according to binomial distribution.
+ * @param min The minimum number to generate. Optional, defaults to 0.
+ * @param max The maximum number to generate. Optional, defaults to 1.
+ * @param skew The skew factor of the binomial distribution. Optional, defaults to 1 (no skew). [more info](https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve)
+ * @returns a randomly generated number, following the binomial distribution.
+ */
+function binomial(min: number = 0, max: number = 1, skew: number = 1) {
+    if (skew <= 0) throw new Error("Skew cannot be lower than or equal to 0");
+
+    let u = 0, v = 0;
+    while (u === 0) u = Math.random() //Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random()
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+    num = num / 10.0 + 0.5 // Translate to 0 -> 1
+    if (num > 1 || num < 0)
+        num = binomial(min, max, skew) // resample between 0 and 1 if out of range
+    else {
+        num = Math.pow(num, skew) // Skew
+        num *= max - min // Stretch to fill range
+        num += min // offset to min
+    }
+    return num
+}
+/**
+ * Chooses a random element from an array, from indexes between `min` and `max`.
+ * @param array The array to choose elements from. Required.
+ * @param min The minimum index to choose from. Optional, defaults to 0.
+ * @param max The maximum index to choose from. Optional, defaults to the last index.
+ * @returns A random element chosen from the array, from indexes between `min` and `max`.
+ */
+function fromArray(array: any[], min: number = 0, max: number = array.length - 1): any {
+    return array[int(min, max)];
+}
+/**
+ * Chooses a random key from an object.
+ * @param object The object to choose keys from. Required.
+ * @returns A random key from the object.
+ */
+function fromObject(object: object): unknown {
+    return fromArray(Object.keys(object));
+}
+/**
+ * Generates a random character from a predefined list. All duplicates in the final list are removed.
+ * @param options Various options to quickly define what characters can be used. The available options are:
+ * * `from` - a string or array of characters to choose from
+ * * `letters` - allows to choose from all lowercase and uppercase letters (a-z and A-Z)
+ * * `uppercase` - allows to choose from all uppercase letters (A-Z)
+ * * `lowercase` - allows to choose from all lowercase letters (a-z)
+ * * `numbers` - allows to choose from all numbers (0-9)
+ * * `special` - allows to choose from all ASCII special characters
+ * @returns A random character chosen from the predefined list.
+ */
+function character(options: { from?: string | string[], letters?: boolean, uppercase?: boolean, lowercase?: boolean, numbers?: boolean, special?: boolean }): string {
+    //convert `options.from` from string to array 
+    if (typeof options.from == "string") options.from = options.from.split('');
+    //initiate array to choose characters from
+    let charArray = options.from ?? [];
+    //add all lowercase letters
+    if (options.letters || options.lowercase) charArray.push(...('abcdefghijklmnopqrstuvwxyz'.split('')));
+    //add all uppercase letters
+    if (options.letters || options.uppercase) charArray.push(...('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')));
+    //add all numbers
+    if (options.numbers) charArray.push(...('0123456789'.split('')));
+    //add all ASCII special characters
+    if (options.special) charArray.push(...('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('')));
+    //remove duplicates, taken from https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array
+    charArray = [...new Set(charArray)];
+    //return
+    return fromArray(charArray);
+}
+/**
+ * Generates A random string of set length from a predefined list of characters.
+ * @param options Various options to quickly define what characters can be used in the string. The available options are:
+ * * `from` - a string or array of characters to choose from
+ * * `letters` - allows to choose from all lowercase and uppercase letters (a-z and A-Z)
+ * * `uppercase` - allows to choose from all uppercase letters (A-Z)
+ * * `lowercase` - allows to choose from all lowercase letters (a-z)
+ * * `numbers` - allows to choose from all numbers (0-9)
+ * * `special` - allows to choose from all ASCII special characters
+ * @returns A random string of characters chosen from the predefined list.
+ */
+function string(length: number, options: { from?: string | string[], letters?: boolean, uppercase?: boolean, lowercase?: boolean, numbers?: boolean, special?: boolean }): string {
+    let str = '';
+    for (let i = 0; i < length; i++) {
+        str += character(options);
+    }
+    return str;
+}
+/**
+ * Generates a random date from between two dates.
+ * @param min The minimum date to choose from. Required.
+ * @param max The maximum date to choose from. Optional, defaults to current date.
+ * @returns A random date from between `min` and `max`.
+ */
+function date(min: Date, max: Date = new Date(Date.now())): Date {
+    return new Date(int(min.valueOf(), max.valueOf()));
+}
+/**
+ * Generates a random string of characters that is a [valid HTML5 id](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/id#sect2).
+ * @param length The length of the returned id. Optional, defaults to 10.
+ * @returns A random string of characters a-z, A-Z, 0-9, "_", and "-".
+ */
+function id(length: number = 10): string {
+    let ret = '';
+    //characters 0-9, "-", and "_" shouldn't be first in the id
+    do {
+        ret = character({ letters: true, numbers: true, from: '-_' });
+    } while (ret <= '9' || ret == '_'); // <= '9' elliminates all numbers and "-", == '_' elliminates "_".
+    //add the rest of the id normally
+    for (let i = 0; i < length; i++) {
+        ret += character({ letters: true, numbers: true, from: '-_' });
+    }
+    return ret;
+}
+/**
+ * Rolls an *n*-sided die and returns the result.
+ * @param n Amount of sides of the die. Optional, defaults to 6.
+ * @returns a random number that may appear on the chosen die. The numbers always range from 1 to `n`.
+ */
+function die(n: number = 6): number {
+    return int(1, n);
+}
+/**
+ * Generates a random RGB color.
+ * @returns An object containing `r`, `g`, and `b` properties, each with a 0-1 number.
+ */
+function rgbColor(): { r: number, g: number, b: number } {
+    return {
+        r: int(0, 255),
+        g: int(0, 255),
+        b: int(0, 255)
+    }
+}
+/**
+ * Generates a random hex color.
+ * @returns A string representing the hex number, with a hash in front.
+ */
+function hexColor(): string {
+    return '#' + string(6, { numbers: true, from: 'abcdef' });
+}
+export const TakeChance = {
+    int,
+    float,
+    multipleInt,
+    multipleFloat,
+    boolean,
+    binomial,
+    character,
+    string,
+    fromArray,
+    fromObject,
+    date,
+    id,
+    die,
+    rgbColor,
+    hexColor,
+}
